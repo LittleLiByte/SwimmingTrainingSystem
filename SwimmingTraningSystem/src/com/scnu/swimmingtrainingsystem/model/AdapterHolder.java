71af40acd8472c70ffbb572fa479f22eec49bdc9
LittleByte <18825111150@163.com>
2015-04-20 12:09:53 +0800

    修改计时界面

--- SwimmingTraningSystem/src/com/scnu/swimmingtrainingsystem/model/AdapterHolder.java
+++
@@ -1,9 +0,0 @@
-package com.scnu.swimmingtrainingsystem.model;
-
-import android.widget.CheckBox;
-import android.widget.TextView;
-
-public final class AdapterHolder {
-	public TextView tv;
-	public CheckBox cb;
-}
